b018cf51a60fda88af0fdca56a53b7836390a5fb
UdayShree123 <udayshree123@gmail.com>
2019-05-20 20:51:14 +0530

    1

---
+++ src/main/java/javapg2/App.java
@@ -0,0 +1,16 @@
+class App
+{
+ public static void main(String args[ ])
+ {
+ float sum,x;
+ int k;
+ sum = 0.0f;
+ for(k =1; k <=5; k++)
+ {
+ x = 1/(float)k;
+ sum = sum + x;
+ System.out.println("value of x:" + x);
+ }
+ System.out.println("sum:" +sum);
+}
+}
